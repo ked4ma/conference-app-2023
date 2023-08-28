5b6d41e70d34940e5010ae505400ff80b5f62eec
ked4ma <hades.netherworld@gmail.com>
2023-08-28 20:09:16 +0900

    trying to add modulegraph

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.androidGradleLibraryPlugin) apply false
     alias(libs.plugins.kotlinGradlePlugin) apply false
     alias(libs.plugins.kotlinxKover) apply false
+    id("dev.iurysouza.modulegraph") version "0.4.0"
 }
 
 tasks.register("clean", Delete::class) {
@@ -19,4 +20,25 @@ buildscript {
             }
         }
     }
+}
+
+moduleGraphConfig {
+    readmePath.set("./README.md")
+    heading.set("### Dependency Diagram")
+//    showFullPath.set(false) // optional
+    orientation.set(dev.iurysouza.modulegraph.Orientation.LEFT_TO_RIGHT) //optional
+    linkText.set(dev.iurysouza.modulegraph.LinkText.NONE) // optional
+    theme.set(dev.iurysouza.modulegraph.Theme.NEUTRAL) // optional
+    // or you can fully customize it by using the BASE theme:
+    // theme.set(Theme.BASE(
+    //      mapOf(
+    //          "primaryTextColor" to "#fff",
+    //          "primaryColor" to "#5a4f7c",
+    //          "primaryBorderColor" to "#5a4f7c",
+    //          "lineColor" to "#f5a623",
+    //          "tertiaryColor" to "#40375c",
+    //          "fontSize" to "11px"
+    //      )
+    //   )
+    // )
 }
